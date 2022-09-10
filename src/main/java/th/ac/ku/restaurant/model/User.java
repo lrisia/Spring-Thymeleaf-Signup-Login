c4a2704b543e667de1acd9dc64c175625ecc4199
Irisia <pochara.su@ku.th>
2022-09-11 01:42:09 +0700

    validation

--- src/main/java/th/ac/ku/restaurant/model/User.java
+++ src/main/java/th/ac/ku/restaurant/model/User.java
@@ -1,10 +1,15 @@
 package th.ac.ku.restaurant.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class User {
 
@@ -16,7 +21,10 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+    private String email;
+
     private String role;
+
 
     //  ให้ Generate..
     //       - Getters และ Setters ทั้งหมด
